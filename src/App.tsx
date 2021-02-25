d2b424aa298c9cafe9e3e8cc8b213d410f22479b
Breno Siqueira <breno.twenty7@gmail.com>
2021-02-25 01:27:04 -0300

    adding experience bar in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ExperienceBar } from './components/ExpreienceBar';
 import './styles/global.css';
+
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className={'container'}>
+      <ExperienceBar />
+    </div >
   );
 }
 
